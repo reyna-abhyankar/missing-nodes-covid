7d93801f3b5b1b2befc17c57c0f69c3355335d27
virenabhyankar <forvirenra@gmail.com>
2020-07-07 01:21:38 -0700

    Draw sample graph

--- helper_scripts/tn93_to_graph.py
+++ helper_scripts/tn93_to_graph.py
@@ -8,7 +8,7 @@ from sys import stdin,stdout
 import networkx as nx
 import argparse
 parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-i', '--input', required=False, type=str, default='stdin', help="Input (tn93 Output CSV")
+parser.add_argument('-i', '--input', required=False, type=str, default='stdin', help="Input tn93")
 parser.add_argument('-t', '--threshold', required=False, type=float, default=float('inf'), help="Distance Threshold")
 parser.add_argument('-o', '--output', required=False, type=str, default='stdout', help="Output File")
 args,unknown = parser.parse_known_args()
@@ -37,3 +37,4 @@ for line in args.input:
     if float(d) > args.threshold:
         continue
     g.add_edge(u, v, weight=d)
+nx.write_gexf(g, 'graph.gexf')
