2b6631253ad01681c7ab0b889aa4b32dec9ac4c0
virenabhyankar <forvirenra@gmail.com>
2020-11-05 01:27:10 -0800

    Add couple of TODOs
    Number of nodes and prefix name

--- Start_to_Finish/dir_to_cascade.py
+++ Start_to_Finish/dir_to_cascade.py
@@ -12,8 +12,8 @@ import os
 import time
 
 def get_num_nodes(path):
-    # maybe put method in contacts files and then pass here?
-    return 100000
+    # TODO: maybe put method in contacts files and then pass here?
+    return 100000   # we currently know how many nodes are in each FAVITES simulation
 
 def main(dir_path, output_dir, fractions):
     script_dir = os.path.dirname(__file__)
@@ -24,9 +24,7 @@ def main(dir_path, output_dir, fractions):
         assert False, "Creation of new folder failed"
 
     output_folder = os.path.join(script_dir, output_dir)
-    print(fractions)
     fractions_list = [float(fraction) for fraction in fractions.split(',')]
-    print(fractions_list)
     # iterate over files in contacts folder
     for f in os.listdir(dir_path):
         path = os.path.join(dir_path, f)
@@ -43,15 +41,18 @@ def main(dir_path, output_dir, fractions):
             if fraction < 0.1 or fraction > 0.9: continue
 
             pkl_dump = favites_to_cascade(cascade, num_nodes, fraction)
+
+            # TODO: better prefix name for file
             new_filename = '%s_%0.1f.pkl' % (f, fraction)
+
             with open(os.path.join(output_folder, new_filename), 'wb') as pkl_file:
                 pkl.dump(pkl_dump, pkl_file) # dump to pkl file format required by tool
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-d', '--input_directory', required=True, type=str, help="Name of FAVITES transmissions folder")
-    parser.add_argument('-f', '--obs_fractions', required=True, type=str, help="Comma-separated input for which observed fractions are desired")
     parser.add_argument('-o', '--output_directory', required=True, type=str, help="Name of new output directory for cascades (.pkl)")
+    parser.add_argument('-f', '--obs_fractions', required=True, type=str, help="Comma-separated input for which observed fractions are desired")
     args,unknown = parser.parse_known_args()
     main(args.input_directory, args.output_directory, args.obs_fractions)
     
